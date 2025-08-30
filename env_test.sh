d726ef44ebf90b8564beec05842c1a1d57049178
yeyeyeping <82635195@qq.com>
2025-08-31 06:34:04 +0800

    update

--- env_test.sh
+++ env_test.sh
@@ -1,2 +1,2 @@
 export EVAL_DATA_PATH=/home/yeep/project/alqq_generc/data/test_data
-export MODEL_OUTPUT_PATH=/home/yeep/project/alqq_generc/working_dir/ckpt/global_step7.loss=9.7861.acc_top1=0.0000.acc_top10=0.0000
+export MODEL_OUTPUT_PATH=/home/yeep/project/alqq_generc/working_dir/ckpt/epoch=1_global_step=7.training_loss=10.4135
