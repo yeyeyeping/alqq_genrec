3852770e4b48afb3e7790f5a5d100f8c52c4ac1c
yeyeyeping <82635195@qq.com>
2025-08-31 16:53:30 +0800

    时间特征

--- env_test.sh
+++ env_test.sh
@@ -1,2 +1,2 @@
 export EVAL_DATA_PATH=/home/yeep/project/alqq_generc/data/test_data
-export MODEL_OUTPUT_PATH=/home/yeep/project/alqq_generc/working_dir/ckpt/epoch=3_global_step=21.training_loss=10.3959
+export MODEL_OUTPUT_PATH=/home/yeep/project/alqq_generc/working_dir/ckpt/epoch=1_global_step=7.training_loss=10.5180
