7737c01915d50d6b5d99f66a8552cd050d7accb6
yeyeyeping <82635195@qq.com>
2025-09-02 08:14:36 +0800

    调整参数

--- const/feature.py
+++ const/feature.py
@@ -72,7 +72,7 @@ class ContextFeature:
             "205",
             "206",
         )
-        self.seq_len = 10
+        self.seq_len = 20
         self.array_feature_ids = ("210", )
         
         self.all_feature_ids = sorted(list(self.sparse_feature_ids) + list(self.array_feature_ids))
